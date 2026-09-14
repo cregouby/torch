a3fcab4d0794a61844684bb311ecc69acd229583
C. Regouby <christophe.regouby@free.fr>
2026-09-14 19:53:21 +0200

    fix #1481 redocument

--- R/distributions-constraints.R
+++ R/distributions-constraints.R
@@ -220,7 +220,7 @@ is_dependent <- function(object) {
       self$upper_bound <- upper_bound
     },
     check = function(value) {
-      self$upper_bound < value
+      value <= self$upper_bound
     },
     print = function() {
       cat(glue::glue(
